1596918b537a7143e7080f25a1c64b6d12239499
xp-network <dima@xpdiem.com>
2021-02-26 18:18:37 +0200

    added cors for development

--- src/config/keys.ts
+++ src/config/keys.ts
@@ -1,3 +1,4 @@
 export const PORT: string = process.env.PORT || '3004'
 export const MONGO_URI: string = process.env.MONGO_URI || 'mongodb://localhost:27017/xpnetwork-application-editor'
 
+export const IS_PROD: boolean = process.env.NODE_ENV === 'production'
