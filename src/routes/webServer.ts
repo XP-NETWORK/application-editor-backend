fd09fa0a1cc176f366f19cc85d98d4d16bd8a35b
xp-network <dima@xpdiem.com>
2021-02-26 17:15:57 +0200

    a

--- src/routes/webServer.ts
+++ src/routes/webServer.ts
@@ -1,8 +1,16 @@
-import { PORT } from '../config/keys'
+import * as mongoose from 'mongoose'
+import { MONGO_URI, PORT } from '../config/keys'
 
 const app = require('express')()
 let http = require('http').Server(app)
 
+mongoose.connect(MONGO_URI, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true  
+}, (err: any) => {
+    if (err) console.log('Error on MongoDB connection', err)
+    else console.log('Connected to MongoDB')
+})
 
 export default http.listen(PORT, () => {
     console.log(`Server runs on ${PORT}`)
