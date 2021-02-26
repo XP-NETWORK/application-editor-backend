95c3b9b6418e92af1d6feec6c4c2083bd857e581
xp-network <dima@xpdiem.com>
2021-02-26 18:16:54 +0200

    added socket io for web socket connectivity

--- src/routes/webServer.ts
+++ src/routes/webServer.ts
@@ -1,5 +1,7 @@
 import * as mongoose from 'mongoose'
 import { MONGO_URI, PORT } from '../config/keys'
+import { Server, Socket } from 'socket.io'
+import websocket from '../websocket/websocket'
 
 const app = require('express')()
 let http = require('http').Server(app)
@@ -11,6 +13,10 @@ mongoose.connect(MONGO_URI, {
     if (err) console.log('Error on MongoDB connection', err)
     else console.log('Connected to MongoDB')
 })
+
+const io = new Server(http, {})
+io.attach(http)
+io.on('connection', (socket: Socket) => websocket(socket))
 
 export default http.listen(PORT, () => {
     console.log(`Server runs on ${PORT}`)
