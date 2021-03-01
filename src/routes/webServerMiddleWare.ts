cbba70ce66778b6d130dac197cc79dbadb11338d
xp-network <dima@xpdiem.com>
2021-03-01 10:18:41 +0200

    added cors for development

--- src/routes/webServerMiddleWare.ts
+++ src/routes/webServerMiddleWare.ts
@@ -3,6 +3,8 @@ import * as bodyParser from 'body-parser'
 import { Express } from 'express'
 import * as express from 'express'
 
+import { IS_PROD } from '../config/keys'
+
 export default (app: Express) => {
     const publicPath = path.join(__dirname, '../../public')
 
@@ -10,4 +12,17 @@ export default (app: Express) => {
     app.use(bodyParser.urlencoded({ extended: true }))
     app.use('/public', express.static(publicPath))
 
+    if(!IS_PROD) app.use((req: any, res: any, next: any) => {
+        res.header("Access-Control-Allow-Origin", "*")
+        res.header("Access-Control-Allow-Headers",
+            "Origin, X-Requeted-With, Content-Type, Accept, Authorization, RBR, access-token")
+        if (req.headers.origin) {
+            res.header('Access-Control-Allow-Origin', '*')
+        }
+        if (req.method === 'OPTIONS') {
+            res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE")
+            return res.status(200).json({})
+        }
+        next()
+    })
 }
