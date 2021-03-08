c36ecd9b09b64f512f3c0725f5a57559c99d03eb
xp-network <dima@xpdiem.com>
2021-03-08 11:23:09 +0200

    added functions for app

--- src/models/app.ts
+++ src/models/app.ts
@@ -31,6 +31,39 @@ schema.set('toObject', {
 })
 
 
+schema.statics.createNew = async function createNew(newDocument: any) {
+    return new this(newDocument).save();
+};
+
+
+schema.statics.getById = async function getById(_id: ObjectId) {
+    const query = this.findOne({ _id });
+    return query.exec().then((doc: any) => (doc ? doc.toJSON() : undefined));
+};
+
+
+schema.statics.removeById = async function removeById(_id: ObjectId) {
+    return new Promise(async (res, rej) => {
+        const query = this.findOneAndRemove({ _id })
+        query.exec().then((err: any, r: any) => {
+            if(err || !r) rej()
+            else res(true)
+        })
+    })
+
+}
+
+schema.statics.updateById = async function updateById(_id: ObjectId, updatedDocument: any) {
+    return new Promise((res, rej) => {
+       const query = this.findByIdAndUpdate({ _id }, updatedDocument, { new: true })
+       query.exec().then((r: any, err: any) => {
+           if(err || !r) rej()
+           else res(r)
+
+       })
+    })
+
+}
 
 const App: IAppModel = model<IAppDocument, IAppModel>('Apps', schema)
 export default App
