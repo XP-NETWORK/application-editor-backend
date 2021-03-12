a5e510d0ae4f7d67cb8cba259f761a632e228aa0
xp-network <dima@xpdiem.com>
2021-03-12 20:25:57 +0200

    added mongodb collections

--- src/config/keys.ts
+++ src/config/keys.ts
@@ -2,3 +2,8 @@ export const PORT: string = process.env.PORT || '3004'
 export const MONGO_URI: string = process.env.MONGO_URI || 'mongodb://localhost:27017/xpnetwork-application-editor'
 
 export const IS_PROD: boolean = process.env.NODE_ENV === 'production'
+
+export const MONGO_COLLECTIONS = {
+    Sections: 'Sections'.toLowerCase(),
+    Pages: 'Pages'.toLowerCase()
+}
