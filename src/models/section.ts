6cc42120638a6c9bb81451f85318ae26ce89e9a5
xp-network <dima@xpdiem.com>
2021-03-15 14:27:50 +0200

    added section model

---
+++ src/models/section.ts
@@ -0,0 +1,25 @@
+import { Document, model, Schema } from 'mongoose'
+import { ObjectId } from 'mongodb'
+import { 
+    ISection, 
+    ISectionDocument, 
+    ISectionModel, 
+} from './interfaces/section'
+import { CustomDocumentBuild } from '../@tools/mongodb/documentDefaults'
+import { MONGO_COLLECTIONS } from '../config/keys'
+
+export const docSection = {
+    SectionId: { type: Schema.Types.ObjectId },
+    index: { type: Schema.Types.Number },
+    type: { type: Schema.Types.String },
+    data: { type: Schema.Types.Mixed } // section's datas to be explained in types
+}
+
+export const schema = CustomDocumentBuild(docSection)
+
+const Section: ISectionModel = model<ISectionDocument, ISectionModel>(MONGO_COLLECTIONS.Sections, schema)
+export default Section
+export {
+    ISection,
+    ISectionDocument
+}
