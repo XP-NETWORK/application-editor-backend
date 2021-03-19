e9158fc43dd472fdf3416d4564533f6a3daf338f
xp-network <dima@xpdiem.com>
2021-03-19 15:28:51 +0200

    added section interface

---
+++ src/models/interfaces/section.ts
@@ -0,0 +1,24 @@
+import { Document, Model } from 'mongoose'
+import { ObjectId } from 'mongodb'
+import { SectionType } from '../../enum/sectionTypeEnum'
+
+export interface ISection {
+    pageId: ObjectId
+    index: number
+    type: SectionType
+    data: any // to be replaced with specific types
+}
+
+// Instance methods
+export interface ISectionDocument extends ISection, Document {
+    toJSON();
+}
+
+
+// Static methods
+export interface ISectionModel extends Model<ISectionDocument> {
+    getById(to: ObjectId): Promise<ISectionDocument>
+    removeById(id: ObjectId): Promise<boolean>
+    updateById(id: ObjectId, doc: ISection): Promise<ISectionDocument>
+    createNew(doc: ISection): Promise<ISectionDocument>
+}
