b4447d148d088401c050489022937259ed808cf2
xp-network <dima@xpdiem.com>
2021-04-06 10:36:34 +0300

    added user login

--- src/models/user/interfaces/user.ts
+++ src/models/user/interfaces/user.ts
@@ -1,7 +1,9 @@
 import { Document, Model } from 'mongoose'
 import { ObjectId } from 'mongodb'
 export const USER_EMAIL_EXISTS = 'This email already exists'
-
+export const USER_DOES_NOT_EXIST = 'User does not exist'
+export const USER_SUCCESSFUL_LOGIN = 'User Successful Login'
+export const USER_LOGIN_INCORRECT_PASSWORD = 'User Incorrect Password'
 export interface IUser {
     password: string
     email: string
--- src/models/user/user.ts
+++ src/models/user/user.ts
@@ -1,9 +1,14 @@
+import  * as bcryptjs from 'bcryptjs'
 import { Document, model, Schema } from 'mongoose'
+
 import { 
     IUser, 
     IUserDocument, 
     IUserModel,
     USER_EMAIL_EXISTS, 
+    USER_SUCCESSFUL_LOGIN,
+    USER_LOGIN_INCORRECT_PASSWORD,
+    USER_DOES_NOT_EXIST
 } from './interfaces/user'
 import { MONGO_COLLECTIONS } from '../../config/keys'
 import { CustomDocumentBuild } from '../../@tools/mongodb/documentDefaults'
@@ -20,6 +25,16 @@ export const schema = CustomDocumentBuild(docUser)
 schema.statics.doesEmailExist = async function doesEmailExist(lowerCaseEmail: string) {
     const query = this.findOne({ lowerCaseEmail })
     return await query.exec().then(r => r ? r : undefined)
+}
+
+schema.statics.login = async function login(email: string, password: string) {
+    const user = await User.doesEmailExist(email.toLowerCase())
+    if(user) {
+        const passwordCorrect: boolean = await bcryptjs.compare(password, user.password)
+        if (passwordCorrect) {
+            return {message: USER_SUCCESSFUL_LOGIN, user: user}
+        } else return {message: USER_LOGIN_INCORRECT_PASSWORD, user}
+    } else return {message: USER_DOES_NOT_EXIST }
 }
 
 schema.statics.register = async function register(user: IUser) {
